00df97e2b7de8a49826199f79c9bed6b3976a82e
PellMell777 <pedrourzuamartelli@gmail.com>
2023-03-08 10:29:21 -0300

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let botonRectangulo = document.getElementById("seleccionRectangulo");
+botonRectangulo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Rectánglo"};
+
+let botonCirculo = document.getElementById("seleccionCirculo");
+botonCirculo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Círculo"};
+
+let botonTriangulo = document.getElementById("seleccionTriangulo")
+botonTriangulo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Triángulo"};
