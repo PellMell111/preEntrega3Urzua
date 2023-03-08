82695bfc1428923e60fb3d995ef4b725c7562c0f
PellMell777 <pedrourzuamartelli@gmail.com>
2023-03-08 12:24:18 -0300

    intento calculo rectangulo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,29 @@
 let botonRectangulo = document.getElementById("seleccionRectangulo");
-botonRectangulo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Rectánglo"};
+botonRectangulo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = `
+    <h2>Rectángulo</h2>
+    <form id="inputRectangulo">
+        <label for="alto">Alto:</label><input id="alto" type="number" name="alto">
+        <label for="ancho">Ancho:</label><input id="ancho" type="number" name="ancho">
+        <input type="button" id="submitRectangulo" value="Calcular">
+    </form>
+    <div id="displayResultado"></div>`};
+
+let alto = document.getElementById("alto").value;
+let ancho = document.getElementById("ancho").value;
+
+let area = alto * ancho
+
+let calcularRectangulo = document.getElementById("submitRectangulo");
+calcularRectangulo.onclick = () => {document.getElementById("displayResultado").innerHTML = `<p>${area}</p>`}
 
 let botonCirculo = document.getElementById("seleccionCirculo");
-botonCirculo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Círculo"};
+botonCirculo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = `
+    <h2>Círculo</h2>
+    <form id="inputCirculo">
+        <label for="radio">Radio:</label><input id="radio" type="number" name="radio">
+        <input type="button" id="submitCirculo" value="Calcular">
+    </form>
+    <div id="displayResultado"></div>`};
 
 let botonTriangulo = document.getElementById("seleccionTriangulo")
 botonTriangulo.onclick = () => {document.getElementById("seleccionFigura").innerHTML = "Triángulo"};
